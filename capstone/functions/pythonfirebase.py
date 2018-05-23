7f54b481409ad79fdc6164105492fb08bded40b2
CMURPH56 <CameronMurphy87@gmail.com>
2018-05-23 16:17:35 -0500

    update pythonfirebase

--- capstone/functions/pythonfirebase.py
+++ capstone/functions/pythonfirebase.py
@@ -9,6 +9,12 @@ Courses = db.reference('Courses')
 '''
 Q. How many courses do you need to graduate?
 A. 19 Courses. 11 Basic Courses. 4 Concentration Courses. 4 Elective Courses. 
+
+Q. The algorithm will have the same shortest path if no classes have been taken.
+A. Yes, The only time the algorithm needs to change is if classes are previously taken
+
+Q. The algorithm for the first 11 courses for each CS concentration might be the same if the
+   there are no other classes taken.
 '''
 
 @app.route('/Courses/<CourseID>')
@@ -24,12 +30,34 @@ def getSpecificCourse(CourseID, ClassID):
 	print(Specific_Class)
 
 
-@app.route('/Courses/<CourseID/<ClassID>/<Attribute>')
+#@app.route('/Courses/<CourseID/<ClassID>/<Attribute>')
 def getCourseAttribute(CourseID, ClassID, Attribute):
 	Concentration_Courses = Courses.child(CourseID)
 	Specific_Class = Concentration_Courses.child(ClassID).get()
 	print(Specific_Class[Attribute])
 
+def fillPreReqs():
+	foundationCoursesList = []
+
+
+def getCourseAvailability(CourseID, ClassID):
+	Concentration_Courses = Courses.child(CourseID)
+	Specific_Class = Concentration_Courses.child(ClassID).get()
+	courseOfferings = [ "Fall 2018-2019", "Spring 2017-2018", "Winter 2017-2018", "Summer II 2016-2017","Spring 2016-2017"]
+	try:	
+		myList = Specific_Class['Years']
+		for date in myList:
+			for offering in pastOfferings: 
+				if date == offering:
+
+					return true
+
+				else:
+
+					return false
+	except KeyError:
+		print("This course is not offered") 
+		return false
 
 
 
@@ -38,7 +66,7 @@ if __name__ == '__main__':
 	#getConcentrationCourses('1')
 	#print("___________ This is the individual Course __________________________")
 	#getSpecificCourse('1', 'CSC 457')
-	getCourseAttribute('1', 'CSC 457', 'CourseName')
-
+	#getCourseAttribute('1', 'CSC 457', 'Years')
+	getCourseAvailability('1', 'CSC 458')		
 
 
